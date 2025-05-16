917d37398a4fbb3795612d5fb46ce4d930202864
nyuiop <gapin.sla@gmail.com>
2025-05-16 12:28:35 +0300

    Update for own casino.bat

--- for own casino.bat
+++ for own casino.bat
@@ -9,7 +9,7 @@ cls & cls && timeout 0 && timeout 0 && timeout 0 && timeout 0
 cls & cls && timeout 0 && timeout 0 && timeout 0 && timeout 0
 cls & cls && timeout 0 && timeout 0 && timeout 0 && timeout 0
 
-set "charset=456321098764355435483475623948756238904573556"
+set "charset=%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%"
 set "length0=1"
 set "randomString0="
 for /l %%a in (1,1,%length0%) do (
@@ -17,7 +17,7 @@ for /l %%a in (1,1,%length0%) do (
     for %%b in (!index!) do set "randomString0=!randomString0!!charset:~%%b,1!"
 )
 
-set "charset=456321098764355435483475623948756238904573556"
+set "charset=%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%"
 set "length1=1"
 set "randomString1="
 for /l %%a in (1,1,%length1%) do (
@@ -26,7 +26,7 @@ for /l %%a in (1,1,%length1%) do (
 )
 
 
-set "charset=0987651234546234564563568725697435696542398745890234975"
+set "charset=%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%"
 set "length2=1"
 set "randomString2="
 for /l %%a in (1,1,%length2%) do (
@@ -34,7 +34,7 @@ for /l %%a in (1,1,%length2%) do (
     for %%b in (!index!) do set "randomString2=!randomString2!!charset:~%%b,1!"
 )
 
-set "charset=0987651234546234564563568725697435696542398745890234975"
+set "charset=%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%"
 set "length3=1"
 set "randomString3="
 for /l %%a in (1,1,%length3%) do (
@@ -43,7 +43,7 @@ for /l %%a in (1,1,%length3%) do (
 )
 
 
-set "charset=0987651234546234564563568725697435696542398745890234975"
+set "charset=%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%"
 set "length4=1"
 set "randomString4="
 for /l %%a in (1,1,%length4%) do (
@@ -52,7 +52,7 @@ for /l %%a in (1,1,%length4%) do (
 )
 
 
-set "charset=237854937252385698203092342734725676125"
+set "charset=%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%"
 set "length5=1"
 set "randomString5="
 for /l %%a in (1,1,%length5%) do (
@@ -60,7 +60,7 @@ for /l %%a in (1,1,%length5%) do (
     for %%b in (!index!) do set "randomString5=!randomString5!!charset:~%%b,1!"
 )
 
-set "charset=237854937252385698203092342734725676125"
+set "charset=%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%"
 set "length6=1"
 set "randomString6="
 for /l %%a in (1,1,%length6%) do (
@@ -69,7 +69,7 @@ for /l %%a in (1,1,%length6%) do (
 )
 
 
-set "charset=125739127836123901236123126751721235345435"
+set "charset=%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%"
 set "length7=1"
 set "randomString7="
 for /l %%a in (1,1,%length7%) do (
@@ -78,7 +78,7 @@ for /l %%a in (1,1,%length7%) do (
 )
 
 
-set "charset=89745698342137568234759023875908234759"
+set "charset=%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%"
 set "length8=1"
 set "randomString8="
 for /l %%a in (1,1,%length8%) do (
@@ -87,7 +87,7 @@ for /l %%a in (1,1,%length8%) do (
 )
 
 
-set "charset=465834658734958490312357889356893745689723"
+set "charset=%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%0%RANDOM%"
 set "length9=1"
 set "randomString9="
 for /l %%a in (1,1,%length9%) do (
